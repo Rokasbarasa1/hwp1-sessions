188e8418a52ae3a40cd3604ac962b595b8240eb9
Rokas <50622827+Rokasbarasa1@users.noreply.github.com>
2022-05-05 21:09:40 +0200

    Finish assignment 4

--- Assignment 4/lib/servo_driver/servo_driver.c
+++ Assignment 4/lib/servo_driver/servo_driver.c
@@ -1,14 +1,28 @@
+/**
+ * @file servo_driver.c
+ * @author Rokas and Arturas (285047@via.dk, 285051@via.dk)
+ * @brief
+ * @version 0.1
+ * @date 2022-05-05
+ *
+ * @copyright Copyright (c) 2022
+ *
+ */
+
 #include "./servo_driver.h"
 
+// No pwm is needed because servos are built to get a single pulse and remember it.
+// Need specifc timed pulse of high and low for the pwm pin of the servo
 void init_servo(){
-    DDRL |= _BV(DDL4);
     //move to 90 (neutral postition)
+    DDRL |= _BV(DDL4);
     PORTL |= _BV(PL4);
     _delay_us(1450);
     PORTL &= ~_BV(PL4);
     _delay_ms(1000);
 }
 
+// Move the servo counter clockwise
 void move_CCW(){
     PORTL |= _BV(PL4);
     _delay_us(2300);
@@ -16,6 +30,7 @@ void move_CCW(){
     _delay_ms(1000);
 }
 
+// Move the servo clockwise
 void move_CW(){
     PORTL |= _BV(PL4);
     _delay_us(600);
